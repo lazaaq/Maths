a00246c05a54c3fecc5c7cd9763f5f053ad222f1
LANA SAIFUL AQIL <70988407+lazaaq@users.noreply.github.com>
2021-03-27 23:39:36 +0700

    Update akar_kuadrat.py

--- akar_kuadrat.py
+++ akar_kuadrat.py
@@ -1,14 +1,17 @@
-import math
-angka = int(input())
-bulat = int(math.sqrt(angka))
-list_satu = []
-for i in range(1,bulat+1):
-    kuadrat = i*i
-    var = int(angka/kuadrat)
-    var1 = angka/kuadrat
-    var2 = var - var1
-    if var2 == 0.0:
-        list_satu.append(var)
-dalam_akar = list_satu[len(list_satu)-1]
-luar_akar = int(math.sqrt(angka/list_satu[len(list_satu)-1]))
-print(luar_akar, "akar", dalam_akar) 
+import math #to import module math (we are gonna using math.sqrt())
+
+user_input = int(input()) #user input (non-negative integer)
+int_input = int(math.sqrt(user_input)) #root
+
+inside_root = root #this is the value outside the root symbol, which has default 1
+for i in range(int_input, 0, -1):
+    square = i*i
+    if int(user_input/square) == user_input/square: 
+        #we are gonna scan from int_input until reach 1, and then stop. when we find this if statement true, 
+        #which is that "i" integer is a square integer, we gonna change the "inside_root" value, and then break the loop
+        inside_root = int(user_input/square)
+        break
+
+outside_root = int(math.sqrt(user_input/root)) #this is the value inside root symbol
+
+print(outside_root, "akar", inside_root)
